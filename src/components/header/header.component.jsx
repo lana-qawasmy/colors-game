c4d49fd9b357d3e5ea3f9d8dcdae0fb66ca7b744
abdullah-nuaimi <181004@ppu.edu.ps>
2022-11-13 13:48:02 +0200

    initialization

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,13 @@
+import './header.css'
+
+
+const Header= () =>{
+    return (
+        <div className="header-container">
+            <span className="game-name">Colors Game</span>
+            <span className="header-steps-counter">Steps: 3</span>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/pages/game-page/game-page.jsx
@@ -0,0 +1,17 @@
+import './game-page.css';
+import Header from '../../components/header/header.component';
+import { useState } from 'react';
+
+
+
+
+const Game =()=>{
+    
+    return <div className="game-container">
+        <Header />
+        <div><hr /></div>
+    </div>;
+}
+
+
+export default Game;
